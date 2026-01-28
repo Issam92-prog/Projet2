e81ac889e841e4611efa54e7088071abb117d312
Alexandre91300saclay <alexandre.fakhfakh@etu-upsaclay.fr>
2026-01-28 16:39:53 +0100

    backend ok , mapping avro aussi , debut de kafka

---
+++ user-service/src/main/kotlin/com/projet2/user/kafka/config/KafkaProducerConfig.kt
@@ -0,0 +1,31 @@
+package com.projet2.user.config
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val configProps = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
+            "schema.registry.url" to "http://localhost:8081"
+        )
+
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+}
